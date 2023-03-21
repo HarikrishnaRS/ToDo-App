86248435e0fa916ad1c926a9dc34a809184d22be
Hari <harikrishna.rs17@gmail.com>
2023-03-21 12:37:33 +0530

    second_commit

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(
-        SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+        const SystemUiOverlayStyle(statusBarColor: Colors.transparent));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'ToDo App',
-      home: Home(),
+      home: Home()  ,
     );
-  }
+  } 
 }
